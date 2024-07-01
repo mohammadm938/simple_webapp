235a77c0abcb1b164611c0ec9492cd5a964829f8
Mohammad Masoudy <imMohammadm938@gmail.com>
2024-07-02 02:29:13 +0430

    u

--- bot.py
+++ bot.py
@@ -2,20 +2,12 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton,WebAppInfo
 
 app = Client("my_bot", api_id="4626556", api_hash="e692aee631de6d8f42d6fe5792810885", bot_token="5052128609:AAGNjLmDrC3u7AFAO7_6DBUiGZxRJieZI3M")
-secret_key = "Mohammad"  # یک کلید مخفی برای امضا کردن JWT
 webapp_site = "https://mohammadm938.github.io/simple_webapp/"
 
 
 
 @app.on_message(filters.command("start"))
 def start(client, message):
-    payload = {
-        "user_id": message.from_user.id,
-        "username": message.from_user.username,
-        "first_name": message.from_user.first_name,
-        "last_name": message.from_user.last_name
-    }
-
     keyboard = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("Open Web App", web_app=WebAppInfo(url=webapp_site))]
@@ -25,6 +17,10 @@ def start(client, message):
     message.reply(
         "Hello! Click the button below to open the web app.",
         reply_markup=keyboard
+    )
+    app.send_message(
+        chat_id=549157192,
+        text="f{message.from_user.first_name} started the bot!",
     )
 
 app.run()
